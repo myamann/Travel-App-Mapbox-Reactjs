f939760502d9f987137d7c820c5208c94e2d726b
Mehmet Yaman <mehmetyaman331@gmail.com>
2021-10-06 15:26:06 +0300

    mapbox setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "100vw",
+    height: "100vh",
+    latitude: 46,
+    longitude: 17,
+    zoom: 4,
+  });
   return (
-    <div >
-     travel pin app
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
     </div>
   );
 }
